0c4b888ee081ae73195c60dbff1727e1a9c06d4c
kundella prasanth yadav <prasanthkdp6@gmail.com>
2020-07-12 21:34:52 +0530

    make it better

---
+++ weather-app/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/weather-app'));
+
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/weather-app/index.html'));
+});
