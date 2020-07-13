d475759514fc5c6b71853ee859f972c81113a095
kundella prasanth yadav <prasanthkdp6@gmail.com>
2020-07-13 06:57:40 +0530

    make it better

--- weather-app/server.js
+++ weather-app/server.js
@@ -5,11 +5,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/weather-app'));
-
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/weather-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
